fa24c9c1f849a6b6435514aa085ceab86844672e
heinrichBlig <heinrichblignaut556@gmail.com>
2021-02-03 17:47:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hello, This is Brandhue Chat app check out our website: http://brandhue.studio/"
 
 @app.route("/sms", methods=['POST'])
 def sms_reply():
@@ -15,8 +15,16 @@ def sms_reply():
 
     # Create reply
     resp = MessagingResponse()
-    resp.message("hahaha")
+    if msg == "1":
+        resp.message(
+            "This is Home bot: we help find you next home ")
+    elif msg == "2":
+        resp.message(
+            "This is Home bot 2: R200 ")
 
+    else:
+        resp.message(
+            "This is Home bot how can we help you \n 1) about us \n 2) cost ")
     return str(resp)
 
 if __name__ == "__main__":
