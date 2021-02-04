8c4c50f25dcdb2e001f72345623cfda4bcfdf873
heinrichBlig <heinrichblignaut556@gmail.com>
2021-02-04 19:10:51 +0200

    update

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello, This is Brandhue Chat app check out our website: http://brandhue.studio/"
+
 
 @app.route("/sms", methods=['POST'])
 def sms_reply():
@@ -15,25 +17,18 @@ def sms_reply():
 
     # Create reply
     resp = MessagingResponse()
-     if msg == "Flats2rent":
+    if msg == "1":
         resp.message(
-                "Hi, whats your name?")
-            if msg !="":
-                 resp.message(
-                     "Hi {}".format(msg) ",  what is your monthly budget? 1. <R500 2. R600 – R1000 3. R1100 – R3000 4. R3000 or more )
-"  )
-            if msg == "1":
-                resp.message(
-                    "This is Home bot: we help find you next home ")
-            elif msg == "2":
-                resp.message(
-                    "This is Home bot 2: R200 ")
+            "This is Home bot: we help find you next home ")
+    elif msg == "2":
+        resp.message(
+            "This is Home bot 2: R200 ")
 
-            else:
-                resp.message(
-                     "Hi, whats your name?")
-        
-        return str(resp)
+    else:
+        resp.message(
+            "Hi, whats your name? ")
+    return str(resp)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
