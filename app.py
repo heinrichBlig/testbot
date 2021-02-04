11219f78b746e294e2595e2a8392fc23f6ff8745
heinrichBlig <heinrichblignaut556@gmail.com>
2021-02-04 20:03:20 +0200

    update

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def sms_reply():
        "Hi, whats your name? ")
         if msg == "Heinrich":
             resp.message(
-                "Hi {}".format(msg) ", what is your monthly budget? 1. <R500 2. R600 – R1000 3. R1100 – R3000 4. R3000 or more ")
+                "Hi {}".format(msg))
 
         if msg == "1":
             resp.message(
