d0cf8c9aeba2ec81af1dc7984aeb6c5b7154e960
heinrichBlig <heinrichblignaut556@gmail.com>
2021-02-04 19:21:22 +0200

    update

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def sms_reply():
     resp = MessagingResponse()
     if msg != "":
         resp.message(
-            "Hi {}".format(msg)", what is your monthly budget? 1. <R500 2. R600 – R1000 3. R1100 – R3000 4. R3000 or more ")
+            "Hi {}".format(msg))
 
     if msg == "1":
         resp.message(
