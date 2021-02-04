b56fd87f4ec10456f49c0192fbcf76fed40c6d6a
heinrichBlig <heinrichblignaut556@gmail.com>
2021-02-04 18:50:23 +0200

    update

--- app.py
+++ app.py
@@ -15,17 +15,27 @@ def sms_reply():
 
     # Create reply
     resp = MessagingResponse()
-    if msg == "1":
+    if msg == "Flats2rent":
         resp.message(
-            "This is Home bot: we help find you next home ")
-    elif msg == "2":
-        resp.message(
-            "This is Home bot 2: R200 ")
+                "Hi, whats your name?")
+            if msg !="":
+                 resp.message(
+                     "Hi {}".format(msg) ',  what is your monthly budget?
+1. <R500
+2. R600 – R1000
+3. R1100 – R3000
+4. R3000 or more'  )
+            if msg == "1":
+                resp.message(
+                    "This is Home bot: we help find you next home ")
+            elif msg == "2":
+                resp.message(
+                    "This is Home bot 2: R200 ")
 
-    else:
-        resp.message(
-            "This is Home bot how can we help you \n 1) about us \n 2) cost ")
-    return str(resp)
+            else:
+                resp.message(
+                    "Hi, whats your name?")
+        return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
