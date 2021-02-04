0e9f21ce647b37349c92bdd5d91712d9b290384d
heinrichBlig <heinrichblignaut556@gmail.com>
2021-02-04 21:47:08 +0200

    update

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello, World!"
+
 
 @app.route("/sms", methods=['POST'])
 def sms_reply():
@@ -15,16 +17,16 @@ def sms_reply():
     # Create reply
     resp = MessagingResponse()
     if msg == "Flats2rent":
-  
         resp.message("Hi, whats your name?")
-        elif msg == "Heinrich":     
-             resp.message("Hi {}".format(msg) + ", what is your monthly budget? \n1. <R500 \n2. R600 – R1000 \n3. R1100 – R3000 \n4. R3000 or more")
-        elif msg == "1":
-             resp.message( "Good, and where do you want to live? \n1. Johannesburg \n2. Pretoria ") 
-   
+    elif msg == "Heinrich":
+        resp.message("Hi {}".format(msg) + ", what is your monthly budget? \n1. <R500 \n2. R600 – R1000 \n3. R1100 – R3000 \n4. R3000 or more")
+    elif msg == "1":
+        resp.message("Good, and where do you want to live? \n1. Johannesburg \n2. Pretoria ")
+
     else:
-       resp.message("Enter Flats2rent to start ") 
+        resp.message("Enter Flats2rent to start ")
     return str(resp)
-        
+
+
 if __name__ == "__main__":
     app.run(debug=True)
