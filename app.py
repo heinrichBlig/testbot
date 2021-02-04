1d963d1a5065b9dfe8e41b8b69f8406752da5b46
heinrichBlig <heinrichblignaut556@gmail.com>
2021-02-04 21:44:26 +0200

    update

--- app.py
+++ app.py
@@ -17,11 +17,10 @@ def sms_reply():
     if msg == "Flats2rent":
   
         resp.message("Hi, whats your name?")
-            
-        resp.message("Hi {}".format(msg) + ", what is your monthly budget? \n1. <R500 \n2. R600 – R1000 \n3. R1100 – R3000 \n4. R3000 or more")
-     elif msg == "1":
-        resp.message(
-            "Good, and where do you want to live? \n1. Johannesburg \n2. Pretoria ") 
+        elif msg == "Heinrich":     
+             resp.message("Hi {}".format(msg) + ", what is your monthly budget? \n1. <R500 \n2. R600 – R1000 \n3. R1100 – R3000 \n4. R3000 or more")
+        elif msg == "1":
+             resp.message( "Good, and where do you want to live? \n1. Johannesburg \n2. Pretoria ") 
    
     else:
        resp.message("Enter Flats2rent to start ") 
