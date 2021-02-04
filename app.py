c46c07bb07971e3327528b18aace2c684beae274
heinrichBlig <heinrichblignaut556@gmail.com>
2021-02-04 22:00:32 +0200

    update

--- app.py
+++ app.py
@@ -14,17 +14,17 @@ def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
     msg = request.form.get('Body')
-    count = 0
     # Create reply
     resp = MessagingResponse()
     if msg == "Flats2rent":
-        count += 1
-        resp.message("Hi, whats your name?" + count)
+        resp.message("Hi, whats your name?")
     elif  msg == "Heinrich":
         resp.message("Hi {}".format(msg) + ", what is your monthly budget? \n1. <R500 \n2. R600 – R1000 \n3. R1100 – R3000 \n4. R3000 or more")
     elif msg == "1":
         resp.message("Good, and where do you want to live? \n1. Johannesburg \n2. Pretoria ")
-
+        if msg == "1":
+            resp.message("Where in JHB do you want to live? \n1.Johannesburg CBD \n2.CBD and surrounds Sandton ")
+            
     
     return str(resp)
 
