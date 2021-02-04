09397c936c813157fbe576aeaa293775c91a2c88
heinrichBlig <heinrichblignaut556@gmail.com>
2021-02-04 21:37:06 +0200

    update

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ def sms_reply():
         counter += 1
         resp.message("Hi, whats your name?")
             
-   elif counter == 2 or counter == 3:
-    resp.message("Hi {}".format(msg) + ", what is your monthly budget? \n1. <R500 \n2. R600 – R1000 \n3. R1100 – R3000 \n4. R3000 or more")
+    elif counter == 2 or counter == 3:
+        resp.message("Hi {}".format(msg) + ", what is your monthly budget? \n1. <R500 \n2. R600 – R1000 \n3. R1100 – R3000 \n4. R3000 or more")
      elif msg == "1":
         resp.message(
             "Good, and where do you want to live? \n1. Johannesburg \n2. Pretoria ") 
