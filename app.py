acf81ff30f3b7f3eba11709ca1332631b2c3ce2f
heinrichBlig <heinrichblignaut556@gmail.com>
2021-02-05 10:30:24 +0200

    update

--- app.py
+++ app.py
@@ -27,22 +27,22 @@ def sms_reply():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
-  @app.route("/sms", methods=['POST'])
-name = "";
-def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Fetch the message
-    msg = request.form.get('Body')
+ 
+ if name  !="":  
+    @app.route("/sms", methods=['POST'])
+        def sms_reply():
+     """Respond to incoming calls with a simple text message."""
+         # Fetch the message
+         msg = request.form.get('Body')
     # Create reply
-    resp = MessagingResponse()
-   if name  !="":
-        resp.message("Hi " + name + ", what is your monthly budget? \n1. <R500 \n2. R600 – R1000 \n3. R1100 – R3000 \n4. R3000 or more")
+        resp = MessagingResponse()
+        if name  !="":
+            resp.message("Hi " + name + ", what is your monthly budget? \n1. <R500 \n2. R600 – R1000 \n3. R1100 – R3000 \n4. R3000 or more")
         
          return str(resp)
         
-    if __name__ == "__main__":
-    app.run(debug=True)
+       if __name__ == "__main__":
+     app.run(debug=True)
 
  #  elif  msg == "Heinrich":
     #   resp.message("Hi {}".format(msg) + ", what is your monthly budget? \n1. <R500 \n2. R600 – R1000 \n3. R1100 – R3000 \n4. R3000 or more")
