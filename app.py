dfb99ce03c5423cf7f044967b61ec360fb2d3bf9
heinrichBlig <heinrichblignaut556@gmail.com>
2021-02-05 10:21:55 +0200

    update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
-
+name = ""
 
 @app.route("/")
 def hello():
@@ -18,13 +18,21 @@ def sms_reply():
     resp = MessagingResponse()
     if msg == "Flats2rent":
         resp.message("Hi, whats your name?")
-   
-    
+       name = msg+1
+  
+    if mgs = name +1:
+        resp.message("Hi {}".format(msg) + ", what is your monthly budget? \n1. <R500 \n2. R600 – R1000 \n3. R1100 – R3000 \n4. R3000 or more")
+        
+    else:
+         resp.message("start conv with 'Flats2rent' ")
     return str(resp)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
+  
+    
 
  #  elif  msg == "Heinrich":
     #   resp.message("Hi {}".format(msg) + ", what is your monthly budget? \n1. <R500 \n2. R600 – R1000 \n3. R1100 – R3000 \n4. R3000 or more")
