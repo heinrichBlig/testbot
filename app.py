5ce400b1353a38ee990576e5bc94876d7711a8d1
heinrichBlig <heinrichblignaut556@gmail.com>
2021-02-05 10:27:30 +0200

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-
+name = "";
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,7 +9,7 @@ def hello():
 
 
 @app.route("/sms", methods=['POST'])
-name = "";
+
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
@@ -18,11 +18,8 @@ def sms_reply():
     resp = MessagingResponse()
     if msg == "Flats2rent":
         resp.message("Hi, whats your name?")
-       name = msg+1
-  
-    if mgs = name +1:
-        resp.message("Hi {}".format(msg) + ", what is your monthly budget? \n1. <R500 \n2. R600 – R1000 \n3. R1100 – R3000 \n4. R3000 or more")
-        
+       name = msg
+         
     else:
          resp.message("start conv with 'Flats2rent' ")
     return str(resp)
@@ -31,8 +28,21 @@ def sms_reply():
 if __name__ == "__main__":
     app.run(debug=True)
     
-  
-    
+  @app.route("/sms", methods=['POST'])
+name = "";
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Fetch the message
+    msg = request.form.get('Body')
+    # Create reply
+    resp = MessagingResponse()
+   if name  !="":
+        resp.message("Hi " + name + ", what is your monthly budget? \n1. <R500 \n2. R600 – R1000 \n3. R1100 – R3000 \n4. R3000 or more")
+        
+         return str(resp)
+        
+    if __name__ == "__main__":
+    app.run(debug=True)
 
  #  elif  msg == "Heinrich":
     #   resp.message("Hi {}".format(msg) + ", what is your monthly budget? \n1. <R500 \n2. R600 – R1000 \n3. R1100 – R3000 \n4. R3000 or more")
